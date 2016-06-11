0e2759ca7a1202ae24130d3686f105248b9e103e
Mopsalarm <mopsalarm@users.noreply.github.com>
2016-06-11 19:38:41 +0200

    Formatting

--- handler.go
+++ handler.go
@@ -1,90 +1,89 @@
 package main
 
 import (
-  "strconv"
-  "net/http"
-  "database/sql"
-  "github.com/rcrowley/go-metrics"
-  "github.com/mopsalarm/go-pr0gramm"
-  "encoding/json"
-  "time"
+	"database/sql"
+	"encoding/json"
+	"github.com/mopsalarm/go-pr0gramm"
+	"github.com/rcrowley/go-metrics"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 type CategoryHandler struct {
-  database *sql.DB
-  timer    metrics.Timer
+	database *sql.DB
+	timer    metrics.Timer
 
-  handle   func(*sql.DB, pr0gramm.ItemsRequest, *http.Request) (pr0gramm.Items, error)
+	handle func(*sql.DB, pr0gramm.ItemsRequest, *http.Request) (pr0gramm.Items, error)
 }
 
 func parseItemRequest(r *http.Request) pr0gramm.ItemsRequest {
-  query := pr0gramm.ItemsRequest{ContentTypes: pr0gramm.ContentTypes{pr0gramm.SFW}}
+	query := pr0gramm.ItemsRequest{ContentTypes: pr0gramm.ContentTypes{pr0gramm.SFW}}
 
-  // parse "older" field
-  if formValue := r.FormValue("older"); formValue != "" {
-    if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
-      query = query.WithOlderThan(pr0gramm.Id(value))
-    }
-  }
+	// parse "older" field
+	if formValue := r.FormValue("older"); formValue != "" {
+		if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
+			query = query.WithOlderThan(pr0gramm.Id(value))
+		}
+	}
 
-  // parse "newer" field
-  if formValue := r.FormValue("newer"); formValue != "" {
-    if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
-      query = query.WithNewerThan(pr0gramm.Id(value))
-    }
-  }
+	// parse "newer" field
+	if formValue := r.FormValue("newer"); formValue != "" {
+		if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
+			query = query.WithNewerThan(pr0gramm.Id(value))
+		}
+	}
 
-  // parse "id" field
-  if formValue := r.FormValue("id"); formValue != "" {
-    if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
-      query = query.WithAround(pr0gramm.Id(value))
-    }
-  }
+	// parse "id" field
+	if formValue := r.FormValue("id"); formValue != "" {
+		if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
+			query = query.WithAround(pr0gramm.Id(value))
+		}
+	}
 
-  // Add a user
-  if formValue := r.FormValue("user"); formValue != "" {
-    query = query.WithUser(formValue)
-  }
+	// Add a user
+	if formValue := r.FormValue("user"); formValue != "" {
+		query = query.WithUser(formValue)
+	}
 
-  if formValue := r.FormValue("likes"); formValue != "" {
-    query = query.WithLikes(formValue)
-  }
+	if formValue := r.FormValue("likes"); formValue != "" {
+		query = query.WithLikes(formValue)
+	}
 
-  if formValue := r.FormValue("tags"); formValue != "" {
-    query = query.WithTag(formValue)
-  }
+	if formValue := r.FormValue("tags"); formValue != "" {
+		query = query.WithTag(formValue)
+	}
 
-  // parse "flags" field
-  if formValue := r.FormValue("flags"); formValue != "" {
-    if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
-      query = query.WithFlags(pr0gramm.ToContentTypes(int(value)))
-    }
-  }
+	// parse "flags" field
+	if formValue := r.FormValue("flags"); formValue != "" {
+		if value, err := strconv.ParseInt(formValue, 10, 0); err == nil {
+			query = query.WithFlags(pr0gramm.ToContentTypes(int(value)))
+		}
+	}
 
-  return query
+	return query
 }
 
 func (ca *CategoryHandler) ServeHTTP(writer http.ResponseWriter, r *http.Request) {
-  ca.timer.Time(func() {
-    startTime := time.Now()
-    itemQuery := parseItemRequest(r)
-    items, err := ca.handle(ca.database, itemQuery, r)
-    if err != nil {
-      writer.WriteHeader(http.StatusInternalServerError)
+	ca.timer.Time(func() {
+		startTime := time.Now()
+		itemQuery := parseItemRequest(r)
+		items, err := ca.handle(ca.database, itemQuery, r)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
 
-    } else {
-      items.QueryCount = 1
-      items.ResponseTime = time.Since(startTime) / time.Millisecond
+		} else {
+			items.QueryCount = 1
+			items.ResponseTime = time.Since(startTime) / time.Millisecond
 
-      result, err := json.Marshal(items)
-      if err != nil {
-        panic(err)
-      }
+			result, err := json.Marshal(items)
+			if err != nil {
+				panic(err)
+			}
 
-      writer.Header().Add("Content-Type", "application/json")
-      writer.Header().Add("Content-Length", strconv.Itoa(len(result)))
-      writer.Write(result)
-    }
-  })
+			writer.Header().Add("Content-Type", "application/json")
+			writer.Header().Add("Content-Length", strconv.Itoa(len(result)))
+			writer.Write(result)
+		}
+	})
 }
-
--- main.go
+++ main.go
@@ -1,77 +1,76 @@
 package main // import "github.com/mopsalarm/go-pr0gramm-categories"
 
 import (
-  "github.com/bobziuchkovski/writ"
-  "os"
-  "database/sql"
-  "log"
-  "github.com/rcrowley/go-metrics"
-  "fmt"
-  "github.com/vistarmedia/go-datadog"
-  "github.com/gorilla/mux"
-  "net/http"
-  "github.com/gorilla/handlers"
-  "time"
-  "github.com/mopsalarm/go-pr0gramm"
-  _ "github.com/lib/pq"
-  "strconv"
-  "strings"
-  "math/rand"
+	"database/sql"
+	"fmt"
+	"github.com/bobziuchkovski/writ"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/mopsalarm/go-pr0gramm"
+	"github.com/rcrowley/go-metrics"
+	"github.com/vistarmedia/go-datadog"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const SAMPLE_PERIOD = time.Minute
 
 type Args struct {
-  HelpFlag bool   `flag:"help" description:"Display this help message and exit"`
-  Port     int    `option:"p, port" default:"8080" description:"The port to open the rest service on"`
-  Postgres string `option:"postgres" default:"host=localhost user=postgres password=password sslmode=disable" description:"Postgres DSN for database connection"`
-  Datadog  string `option:"datadog" description:"Datadog api key for reporting"`
+	HelpFlag bool   `flag:"help" description:"Display this help message and exit"`
+	Port     int    `option:"p, port" default:"8080" description:"The port to open the rest service on"`
+	Postgres string `option:"postgres" default:"host=localhost user=postgres password=password sslmode=disable" description:"Postgres DSN for database connection"`
+	Datadog  string `option:"datadog" description:"Datadog api key for reporting"`
 }
-
 
 // Literal escape the given string.
 func EscapeDbString(s string) string {
-  p := ""
+	p := ""
 
-  if strings.Contains(s, `\`) {
-    p = "E"
-  }
+	if strings.Contains(s, `\`) {
+		p = "E"
+	}
 
-  s = strings.Replace(s, `'`, `''`, -1)
-  s = strings.Replace(s, `\`, `\\`, -1)
-  return p + `'` + s + `'`
+	s = strings.Replace(s, `'`, `''`, -1)
+	s = strings.Replace(s, `\`, `\\`, -1)
+	return p + `'` + s + `'`
 }
 
 func scanItemsFromCursor(rows *sql.Rows, req pr0gramm.ItemsRequest) pr0gramm.Items {
-  itemList := make([]pr0gramm.Item, 0, 120)
-  for rows.Next() {
-    // get the stuff from the cursor
-    var created int64
-    var item pr0gramm.Item
-    err := rows.Scan(&item.Id, &item.Promoted, &item.Up, &item.Down, &item.Flags,
-      &item.Image, &item.Source, &item.Thumbnail, &item.Fullsize,
-      &item.User, &item.Mark, &created)
+	itemList := make([]pr0gramm.Item, 0, 120)
+	for rows.Next() {
+		// get the stuff from the cursor
+		var created int64
+		var item pr0gramm.Item
+		err := rows.Scan(&item.Id, &item.Promoted, &item.Up, &item.Down, &item.Flags,
+			&item.Image, &item.Source, &item.Thumbnail, &item.Fullsize,
+			&item.User, &item.Mark, &created)
 
-    if err != nil {
-      panic(err)
-    }
+		if err != nil {
+			panic(err)
+		}
 
-    item.Created = pr0gramm.Timestamp{time.Unix(created, 0)}
-    itemList = append(itemList, item)
-  }
+		item.Created = pr0gramm.Timestamp{time.Unix(created, 0)}
+		itemList = append(itemList, item)
+	}
 
-  return pr0gramm.Items{
-    Response: pr0gramm.Response{
-      Timestamp: pr0gramm.Timestamp{time.Now()},
-    },
-    Items: itemList,
-    AtEnd: len(itemList) < 120,
-    AtStart: req.Older == 0,
-  }
+	return pr0gramm.Items{
+		Response: pr0gramm.Response{
+			Timestamp: pr0gramm.Timestamp{time.Now()},
+		},
+		Items:   itemList,
+		AtEnd:   len(itemList) < 120,
+		AtStart: req.Older == 0,
+	}
 }
 
 func HandleControversial(db *sql.DB, req pr0gramm.ItemsRequest, r *http.Request) (pr0gramm.Items, error) {
-  rows, err := db.Query(`
+	rows, err := db.Query(`
     SELECT items.id, items.promoted, items.up, items.down, items.flags,
       items.image, items.source, items.thumb, items.fullsize,
       items.username, items.mark, items.created
@@ -85,47 +84,47 @@ func HandleControversial(db *sql.DB, req pr0gramm.ItemsRequest, r *http.Request)
     )
     ORDER BY controversial.id DESC LIMIT 120`, req.ContentTypes.AsFlags(), req.Older, req.Newer)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  defer rows.Close()
-  return scanItemsFromCursor(rows, req), nil
+	defer rows.Close()
+	return scanItemsFromCursor(rows, req), nil
 }
 
 func HandleBestOf(db *sql.DB, req pr0gramm.ItemsRequest, r *http.Request) (pr0gramm.Items, error) {
-  var minScore = 2000
+	var minScore = 2000
 
-  // parse min score value, if present and valid.
-  if minScoreStr := r.FormValue("score"); minScoreStr != "" {
-    if value, err := strconv.Atoi(minScoreStr); err == nil {
-      minScore = value
-    }
-  }
+	// parse min score value, if present and valid.
+	if minScoreStr := r.FormValue("score"); minScoreStr != "" {
+		if value, err := strconv.Atoi(minScoreStr); err == nil {
+			minScore = value
+		}
+	}
 
-  // start building the query
-  qJoins := []string{"JOIN items ON items_bestof.id=items.id" }
-  qWhere := []string{fmt.Sprintf("items_bestof.score > %d", minScore) }
+	// start building the query
+	qJoins := []string{"JOIN items ON items_bestof.id=items.id"}
+	qWhere := []string{fmt.Sprintf("items_bestof.score > %d", minScore)}
 
-  if req.Tags != nil {
-    term := strings.Join(strings.Split(strings.Replace(*req.Tags, "&", "", -1), " "), " & ")
-    term = EscapeDbString(term)
+	if req.Tags != nil {
+		term := strings.Join(strings.Split(strings.Replace(*req.Tags, "&", "", -1), " "), " & ")
+		term = EscapeDbString(term)
 
-    qJoins = append(qJoins, "JOIN tags ON items_bestof.id=tags.item_id")
-    qWhere = append(qWhere, fmt.Sprintf(
-      "to_tsvector('simple', tags.tag) @@ to_tsquery('simple', %s)", term))
-  }
+		qJoins = append(qJoins, "JOIN tags ON items_bestof.id=tags.item_id")
+		qWhere = append(qWhere, fmt.Sprintf(
+			"to_tsvector('simple', tags.tag) @@ to_tsquery('simple', %s)", term))
+	}
 
-  if req.ContentTypes.AsFlags() != 7 {
-    qWhere = append(qWhere, fmt.Sprintf("items.flags & %d != 0", req.ContentTypes.AsFlags()))
-  }
+	if req.ContentTypes.AsFlags() != 7 {
+		qWhere = append(qWhere, fmt.Sprintf("items.flags & %d != 0", req.ContentTypes.AsFlags()))
+	}
 
-  if req.User != nil {
-    name := strings.ToLower(*req.User)
-    qWhere = append(qWhere, "lower(items.username)=" + EscapeDbString(name))
-  }
+	if req.User != nil {
+		name := strings.ToLower(*req.User)
+		qWhere = append(qWhere, "lower(items.username)="+EscapeDbString(name))
+	}
 
-  query := fmt.Sprintf(`
+	query := fmt.Sprintf(`
     SELECT DISTINCT ON (items_bestof.id)
       items.id, items.promoted, items.up, items.down, items.flags,
       items.image, items.source, items.thumb, items.fullsize,
@@ -136,81 +135,81 @@ func HandleBestOf(db *sql.DB, req pr0gramm.ItemsRequest, r *http.Request) (pr0gr
       AND ($2 = 0 OR items.id > $2)
     ORDER BY items_bestof.id DESC LIMIT 120
     `,
-    strings.Join(qJoins, " "),
-    strings.Join(qWhere, " AND "))
+		strings.Join(qJoins, " "),
+		strings.Join(qWhere, " AND "))
 
-  rows, err := db.Query(query, req.Older, req.Newer)
-  if err != nil {
-    panic(err)
-  }
+	rows, err := db.Query(query, req.Older, req.Newer)
+	if err != nil {
+		panic(err)
+	}
 
-  defer rows.Close()
-  return scanItemsFromCursor(rows, req), nil
+	defer rows.Close()
+	return scanItemsFromCursor(rows, req), nil
 }
 
 func HandleRandom(db *sql.DB, req pr0gramm.ItemsRequest, r *http.Request) (pr0gramm.Items, error) {
-  var rows *sql.Rows
-  var err error
+	var rows *sql.Rows
+	var err error
 
-  // execute the correct query
-  flags := req.ContentTypes.AsFlags()
-  if flags == 4 {
-    rows, err = db.Query(QueryRandomNsfl)
-  } else {
-    rows, err = db.Query(QueryRandomRest, flags)
-  }
+	// execute the correct query
+	flags := req.ContentTypes.AsFlags()
+	if flags == 4 {
+		rows, err = db.Query(QueryRandomNsfl)
+	} else {
+		rows, err = db.Query(QueryRandomRest, flags)
+	}
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  defer rows.Close()
-  result := scanItemsFromCursor(rows, req)
+	defer rows.Close()
+	result := scanItemsFromCursor(rows, req)
 
-  // shuffle
-  for i := range result.Items {
-    j := rand.Intn(i + 1)
-    result.Items[i], result.Items[j] = result.Items[j], result.Items[i]
-  }
+	// shuffle
+	for i := range result.Items {
+		j := rand.Intn(i + 1)
+		result.Items[i], result.Items[j] = result.Items[j], result.Items[i]
+	}
 
-  result.AtEnd = len(result.Items) > 60
-  return result, nil
+	result.AtEnd = len(result.Items) > 60
+	return result, nil
 }
 
 func parseArguments() *Args {
-  args := &Args{}
-  cmd := writ.New("categories", args)
+	args := &Args{}
+	cmd := writ.New("categories", args)
 
-  // Parse command line arguments
-  _, _, err := cmd.Decode(os.Args[1:])
-  if err != nil || args.HelpFlag {
-    cmd.ExitHelp(err)
-  }
+	// Parse command line arguments
+	_, _, err := cmd.Decode(os.Args[1:])
+	if err != nil || args.HelpFlag {
+		cmd.ExitHelp(err)
+	}
 
-  return args
+	return args
 }
 
 // just send http ok
 func ping(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
 
 func UpdateNsflView(db *sql.DB) {
-  go UpdateNsflViewOnce(db)
-  for range time.Tick(6 * time.Hour) {
-    go UpdateNsflViewOnce(db)
-  }
+	go UpdateNsflViewOnce(db)
+	for range time.Tick(6 * time.Hour) {
+		go UpdateNsflViewOnce(db)
+	}
 }
 
 func UpdateNsflViewOnce(db *sql.DB) {
-  log.Println("Updating nsfl view")
+	log.Println("Updating nsfl view")
 
-  tx, err := db.Begin()
-  if err != nil {
-    panic(err)
-  }
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
 
-  _, err = tx.Exec(`
+	_, err = tx.Exec(`
     -- create view with only nsfl posts
     CREATE MATERIALIZED VIEW IF NOT EXISTS random_items_nsfl (id, promoted) AS (
       SELECT id, promoted FROM items WHERE flags=4);
@@ -221,54 +220,54 @@ func UpdateNsflViewOnce(db *sql.DB) {
     -- refresh the nsfl items.
     REFRESH MATERIALIZED VIEW CONCURRENTLY random_items_nsfl;`)
 
-  if err != nil {
-    tx.Rollback()
-    panic(err)
-  } else {
-    log.Println("Updating nsfl view completed")
-    tx.Commit()
-  }
+	if err != nil {
+		tx.Rollback()
+		panic(err)
+	} else {
+		log.Println("Updating nsfl view completed")
+		tx.Commit()
+	}
 }
 
 func main() {
-  args := parseArguments()
+	args := parseArguments()
 
-  // open database connection
-  db, err := sql.Open("postgres", args.Postgres)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// open database connection
+	db, err := sql.Open("postgres", args.Postgres)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  db.SetMaxOpenConns(4)
+	db.SetMaxOpenConns(4)
 
-  // check if it is valid
-  if err = db.Ping(); err != nil {
-    log.Fatal(err)
-  }
+	// check if it is valid
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 
-  // get info about the runtime every few seconds
-  metrics.RegisterRuntimeMemStats(metrics.DefaultRegistry)
-  go metrics.CaptureRuntimeMemStats(metrics.DefaultRegistry, SAMPLE_PERIOD)
+	// get info about the runtime every few seconds
+	metrics.RegisterRuntimeMemStats(metrics.DefaultRegistry)
+	go metrics.CaptureRuntimeMemStats(metrics.DefaultRegistry, SAMPLE_PERIOD)
 
-  if len(args.Datadog) > 0 {
-    host, _ := os.Hostname()
+	if len(args.Datadog) > 0 {
+		host, _ := os.Hostname()
 
-    fmt.Printf("Starting datadog reporter on host %s\n", host)
-    go datadog.New(host, args.Datadog).DefaultReporter().Start(SAMPLE_PERIOD)
-  }
+		fmt.Printf("Starting datadog reporter on host %s\n", host)
+		go datadog.New(host, args.Datadog).DefaultReporter().Start(SAMPLE_PERIOD)
+	}
 
-  go UpdateNsflView(db)
+	go UpdateNsflView(db)
 
-  router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter().StrictSlash(true)
 
-  timer := metrics.NewRegisteredTimer("pr0gramm.categories.controversial.update", nil)
-  router.Handle("/controversial", &CategoryHandler{db, timer, HandleControversial})
-  router.Handle("/bestof", &CategoryHandler{db, timer, HandleBestOf})
-  router.Handle("/random", &CategoryHandler{db, timer, HandleRandom})
-  router.HandleFunc("/ping", ping)
+	timer := metrics.NewRegisteredTimer("pr0gramm.categories.controversial.update", nil)
+	router.Handle("/controversial", &CategoryHandler{db, timer, HandleControversial})
+	router.Handle("/bestof", &CategoryHandler{db, timer, HandleBestOf})
+	router.Handle("/random", &CategoryHandler{db, timer, HandleRandom})
+	router.HandleFunc("/ping", ping)
 
-  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", args.Port),
-    handlers.RecoveryHandler()(
-      handlers.LoggingHandler(os.Stdout,
-        handlers.CORS()(router)))))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", args.Port),
+		handlers.RecoveryHandler()(
+			handlers.LoggingHandler(os.Stdout,
+				handlers.CORS()(router)))))
 }
